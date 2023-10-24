07c4bc80e7be4523ee866e9a8abfc7f514ceaedc
MainaJoseph <mainajm254@gmail.com>
2023-10-24 11:44:13 +0300

    Added Redressed to Navbar icon

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import Container from "../Container";
+import { Redressed } from "next/font/google";
+
+const redressed = Redressed({subsets: ['latin'], weight: ['400']})
 
 const NavBar = () => {
     return ( <div
@@ -14,7 +17,7 @@ const NavBar = () => {
      <div className="py-4 border-b-[1px]"> 
      <Container>
         <div className="flex items-center justify-between gap-3 md:gap-0">
-           <Link  href='/'>Nova</Link> 
+           <Link  href='/' className={`${redressed.className} font-bold text-2xl`}>Nova</Link> 
            <div className="hidden md:block">Search</div>
            <div className="flex items-center gap-8 md:gap-12">
             <div>CartCount</div>
