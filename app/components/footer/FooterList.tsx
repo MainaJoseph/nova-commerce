d4a66b5e9e0d93319f143848951b00b20695603c
MainaJoseph <mainajm254@gmail.com>
2023-10-24 11:52:38 +0300

    Added FooterList.tsx

---
+++ app/components/footer/FooterList.tsx
@@ -0,0 +1,13 @@
+interface FooterListProps{
+    children: React.ReactNode;
+}
+
+const FooterList: React.FC<FooterListProps> = ({children}) => {
+
+    return ( <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/6 mb-6 flex flex-col gap-2">
+            {children}
+    </div> 
+    );
+}
+ 
+export default FooterList;
