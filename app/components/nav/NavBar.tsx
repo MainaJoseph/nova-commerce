17838573f57f3c585b29317382996733751fd312
MainaJoseph <mainajm254@gmail.com>
2023-10-24 11:34:08 +0300

    Updated Navbar

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,6 +1,29 @@
+import Link from "next/link";
+import Container from "../Container";
+
 const NavBar = () => {
-    return ( <div>
-        Navbar
+    return ( <div
+    className="
+    sticky
+    top-0
+    w-full
+    bg-slate-200
+    z-30
+    shadow-sm
+    ">
+     <div className="py-4 border-b-[1px]"> 
+     <Container>
+        <div className="flex items-center justify-between gap-3 md:gap-0">
+           <Link  href='/'>Nova</Link> 
+           <div className="hidden md:block">Search</div>
+           <div className="flex items-center gap-8 md:gap-12">
+            <div>CartCount</div>
+            <div>Usermenu</div>
+           </div>
+        </div>
+     </Container>
+
+        </div>
     </div>);
 }
  
