3ff7595eb83d63621f2a4e8160e0b13f949c2862
MainaJoseph <mainajm254@gmail.com>
2023-10-25 08:00:12 +0300

    Finished the footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -16,77 +16,77 @@ const Footer = () => {
             <FooterList>
                 <h3 className="font-bold mb-2 text-sm">
                     Need Help?</h3>
-                    <Link href='#'> Chat with Us</Link>
-                    <Link href='#'> Help Center</Link>
-                    <Link href='#'> Contact Us</Link>
+                    <Link href='#' className="hover:underline"> Chat with Us</Link>
+                    <Link href='#'className="hover:underline"> Help Center</Link>
+                    <Link href='#'className="hover:underline"> Contact Us</Link>
 
                     <h3 className="text-sm font-bold mb-2 mt-2">
                     USEFUL LINKS</h3>
-                    <Link href='#'>Track Your Order </Link>
-                    <Link href='#'>Shipping and delivery</Link>
-                    <Link href='#'> Return Policy</Link>
-                    <Link href='#'> How to Order?</Link>
-                    <Link href='#'> Dispute Resolution Policy</Link>
-                    <Link href='#'> Corporate and Bulk Purchase</Link>
-                    <Link href='#'> Advertise with Nova</Link>
-                    <Link href='#'> Report a Product</Link>
-                    <Link href='#'> Jumia Payment Information Guidelines</Link>
+                    <Link href='#'className="hover:underline">Track Your Order </Link>
+                    <Link href='#'className="hover:underline">Shipping and delivery</Link>
+                    <Link href='#'className="hover:underline"> Return Policy</Link>
+                    <Link href='#'className="hover:underline"> How to Order?</Link>
+                    <Link href='#'className="hover:underline"> Dispute Resolution Policy</Link>
+                    <Link href='#'className="hover:underline"> Corporate and Bulk Purchase</Link>
+                    <Link href='#'className="hover:underline"> Advertise with Nova</Link>
+                    <Link href='#'className="hover:underline"> Report a Product</Link>
+                    <Link href='#'className="hover:underline"> Jumia Payment Information Guidelines</Link>
                     
             </FooterList>
             <FooterList>
                 <h3 className="text-sm font-bold mb-2">
                     About Nova</h3>
-                    <Link href='#'> About us</Link>
-                    <Link href='#'> Returns and Refunds Policy</Link>
-                    <Link href='#'> Nova Careers</Link>
-                    <Link href='#'> Nova Express</Link>
-                    <Link href='#'> Terms and Conditions</Link>
-                    <Link href='#'> Store Credit Terms and Conditions</Link>
-                    <Link href='#'> Privacy Notice</Link>
-                    <Link href='#'> Cookies Notice</Link>
-                    <Link href='#'> Flash Sales</Link>
-                    <Link href='#'> Black Friday 2023</Link>
+                    <Link href='#'className="hover:underline"> About us</Link>
+                    <Link href='#'className="hover:underline"> Returns and Refunds Policy</Link>
+                    <Link href='#'className="hover:underline"> Nova Careers</Link>
+                    <Link href='#'className="hover:underline"> Nova Express</Link>
+                    <Link href='#'className="hover:underline"> Terms and Conditions</Link>
+                    <Link href='#'className="hover:underline"> Store Credit Terms and Conditions</Link>
+                    <Link href='#'className="hover:underline"> Privacy Notice</Link>
+                    <Link href='#'className="hover:underline"> Cookies Notice</Link>
+                    <Link href='#'className="hover:underline"> Flash Sales</Link>
+                    <Link href='#'className="hover:underline"> Black Friday 2023</Link>
                    
             </FooterList>
 
             <FooterList>
                 <h3 className="text-sm font-bold mb-2">
                     Make Money with Nova</h3>
-                    <Link href='#'> Sell on Nova</Link>
-                    <Link href='#'> Vendor Hub</Link>
-                    <Link href='#'> Become a Sales Consultant</Link>
-                    <Link href='#'> Become a Logistics Service Partner</Link>
-                    <Link href='#'> Nova City Partner Program</Link>
+                    <Link href='#'className="hover:underline"> Sell on Nova</Link>
+                    <Link href='#'className="hover:underline"> Vendor Hub</Link>
+                    <Link href='#'className="hover:underline"> Become a Sales Consultant</Link>
+                    <Link href='#'className="hover:underline"> Become a Logistics Service Partner</Link>
+                    <Link href='#'className="hover:underline"> Nova City Partner Program</Link>
 
-                    <p className="mt-4">&copy; {new Date().getFullYear()} Nova. All rights reserved</p>
+                    <p className="mt-4 hover:text-orange-500">&copy; {new Date().getFullYear()} Nova. All rights reserved</p>
             </FooterList>
            
             <FooterListCol>
-  <h3 className="text-sm font-bold mb-2">NOVA INTERNATIONAL</h3>
+  <h3 className="text-sm font-bold mb-2 ">NOVA INTERNATIONAL</h3>
   <div className="flex flex-wrap">
     <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 mb-2">
-      <Link href='#'>Algeria</Link>
+      <Link href='#'className="hover:underline">Algeria</Link>
     </div>
     <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 mb-2">
-      <Link href='#'>Tanzania</Link>
+      <Link href='#'className="hover:underline">Tanzania</Link>
     </div>
     <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 mb-2">
-      <Link href='#'>Uganda</Link>
+      <Link href='#'className="hover:underline">Uganda</Link>
     </div>
     <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 mb-2">
-      <Link href='#'>Tunisia</Link>
+      <Link href='#'className="hover:underline">Tunisia</Link>
     </div>
     <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 mb-2">
-      <Link href='#'>Morocco</Link>
+      <Link href='#'className="hover:underline">Morocco</Link>
     </div>
     <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 mb-2">
-      <Link href='#'>Ivory Coast</Link>
+      <Link href='#'className="hover:underline">Ivory Coast</Link>
     </div>
     <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 mb-2">
-      <Link href='#'>Zando</Link>
+      <Link href='#'className="hover:underline">Zando</Link>
     </div>
     <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 mb-2">
-      <Link href='#'>Sudan</Link>
+      <Link href='#'className="hover:underline">Sudan</Link>
     </div>
   </div>
 </FooterListCol>
@@ -104,15 +104,15 @@ const Footer = () => {
                     Join Us</h3>
                     <div className="flex gap-3">
                     <Link href='#'>
-                      <FiFacebook size={19} className="mt-1" />
+                      <FiFacebook size={19} className="mt-1 hover:text-orange-500" />
                     </Link>
                     <Link href='#'>
-                      <RiTwitterXFill size={19} className="mt-1" />
+                      <RiTwitterXFill size={19} className="mt-1 hover:text-orange-500" />
                     </Link>
-                    <Link href='#'>
+                    <Link href='#' className="hover:text-orange-500">
                       <AiFillInstagram size={24} />
                     </Link>
-                    <Link href='#'>
+                    <Link href='#' className="hover:text-orange-500">
                       <AiFillYoutube size={24} />
                     </Link>
                     </div>
@@ -124,17 +124,17 @@ const Footer = () => {
             <FooterList>
             <h3 className="text-sm font-bold mb-2">
                     Payment Methods</h3>
-                    <div className="flex gap-3">
+                    <div className="flex gap-3 ">
                     <Link href='#'>
-                      <GiReceiveMoney size={24} />
+                      <GiReceiveMoney size={24} className="hover:text-orange-500"/>
                     </Link>
                     <Link href='#'>
-                      <RiVisaLine size={24} />
+                      <RiVisaLine size={24} className="hover:text-orange-500" />
                     </Link>
                     <Link href='#'>
-                      <FaCcMastercard size={24} />
+                      <FaCcMastercard size={24} className="hover:text-orange-500" />
                     </Link>
-                    <Link href='#' style={{fontSize: "12px"}} className="mt-1 flex flex-row">
+                    <Link href='#' style={{fontSize: "12px"}} className="mt-1 flex flex-row hover:text-orange-500">
                       Nova <AiOutlineSlack size={19} /> Pay
                     </Link>
                     
@@ -161,62 +161,62 @@ const Footer = () => {
 
         <div className="flex flex-row  justify-between   pt-0 pb-8 ">
         <FooterList>
-                    <Link href='#'> Adidas</Link>
-                    <Link href='#'> AILYONS</Link>
-                    <Link href='#'> Apple</Link>
-                    <Link href='#'> Asus</Link>
-                    <Link href='#'> Bruhm</Link>
-                    <Link href='#'> Canon</Link>
+                    <Link href='#'className="hover:underline"> Adidas</Link>
+                    <Link href='#'className="hover:underline"> AILYONS</Link>
+                    <Link href='#'className="hover:underline"> Apple</Link>
+                    <Link href='#'className="hover:underline"> Asus</Link>
+                    <Link href='#'className="hover:underline"> Bruhm</Link>
+                    <Link href='#'className="hover:underline"> Canon</Link>
             </FooterList>
         <FooterList>
-                    <Link href='#'> Cantu</Link>
-                    <Link href='#'> Coke</Link>
-                    <Link href='#'> Dell</Link>
-                    <Link href='#'> Dove</Link>
-                    <Link href='#'> Dr.Rashel</Link>
+                    <Link href='#'className="hover:underline"> Cantu</Link>
+                    <Link href='#'className="hover:underline"> Coke</Link>
+                    <Link href='#'className="hover:underline"> Dell</Link>
+                    <Link href='#'className="hover:underline"> Dove</Link>
+                    <Link href='#'className="hover:underline"> Dr.Rashel</Link>
                     
             </FooterList>
         <FooterList>
-                    <Link href='#'> Epson</Link>
-                    <Link href='#'> Garnier</Link>
-                    <Link href='#'> Hisense</Link>
-                    <Link href='#'> HP</Link>
-                    <Link href='#'> Infinix</Link>
+                    <Link href='#'className="hover:underline"> Epson</Link>
+                    <Link href='#'className="hover:underline"> Garnier</Link>
+                    <Link href='#'className="hover:underline"> Hisense</Link>
+                    <Link href='#'className="hover:underline"> HP</Link>
+                    <Link href='#'className="hover:underline"> Infinix</Link>
             </FooterList>
         <FooterList>
-                    <Link href='#'> itel</Link>
-                    <Link href='#'> Jameson</Link>
-                    <Link href='#'> L Oréal Paris</Link>
-                    <Link href='#'> Lenovo</Link>
-                    <Link href='#'> Logitech</Link>
+                    <Link href='#'className="hover:underline"> itel</Link>
+                    <Link href='#'className="hover:underline"> Jameson</Link>
+                    <Link href='#'className="hover:underline"> L Oréal Paris</Link>
+                    <Link href='#'className="hover:underline"> Lenovo</Link>
+                    <Link href='#'className="hover:underline"> Logitech</Link>
             </FooterList>
         <FooterList>
-                    <Link href='#'> Maybeline</Link>
-                    <Link href='#'> Mika</Link>
-                    <Link href='#'> NIVEA</Link>
-                    <Link href='#'> Nunix</Link>
-                    <Link href='#'> Oppo</Link>
+                    <Link href='#'className="hover:underline"> Maybeline</Link>
+                    <Link href='#'className="hover:underline"> Mika</Link>
+                    <Link href='#'className="hover:underline"> NIVEA</Link>
+                    <Link href='#'className="hover:underline"> Nunix</Link>
+                    <Link href='#'className="hover:underline"> Oppo</Link>
             </FooterList>
         <FooterList>
-                    <Link href='#'> Ramtons</Link>
-                    <Link href='#'> Roch</Link>
-                    <Link href='#'> Samsung</Link>
-                    <Link href='#'> Sandisk</Link>
-                    <Link href='#'> Skyworth</Link>
+                    <Link href='#'className="hover:underline"> Ramtons</Link>
+                    <Link href='#'className="hover:underline"> Roch</Link>
+                    <Link href='#'className="hover:underline"> Samsung</Link>
+                    <Link href='#'className="hover:underline"> Sandisk</Link>
+                    <Link href='#'className="hover:underline"> Skyworth</Link>
             </FooterList>
         <FooterList>
-                    <Link href='#'> Sony</Link>
-                    <Link href='#'> TCL</Link>
-                    <Link href='#'> Tecno</Link>
-                    <Link href='#'> Transcend</Link>
-                    <Link href='#'> Tusker</Link>
+                    <Link href='#'className="hover:underline"> Sony</Link>
+                    <Link href='#'className="hover:underline"> TCL</Link>
+                    <Link href='#'className="hover:underline"> Tecno</Link>
+                    <Link href='#'className="hover:underline"> Transcend</Link>
+                    <Link href='#'className="hover:underline"> Tusker</Link>
             </FooterList>
         <FooterList>
-                    <Link href='#'> USN</Link>
-                    <Link href='#'> Vision Plus</Link>
-                    <Link href='#'> Vitron</Link>
-                    <Link href='#'> VON</Link>
-                    <Link href='#'> XIAOMI</Link>
+                    <Link href='#'className="hover:underline"> USN</Link>
+                    <Link href='#'className="hover:underline"> Vision Plus</Link>
+                    <Link href='#'className="hover:underline"> Vitron</Link>
+                    <Link href='#'className="hover:underline"> VON</Link>
+                    <Link href='#'className="hover:underline"> XIAOMI</Link>
             </FooterList>
         </div>
 
@@ -225,11 +225,11 @@ const Footer = () => {
 
         <div className="flex flex-row justify-center pt-3 pb-8 gap-6 ">
 
-        <Link href='#' style={{fontSize: "13px"}} className=" flex flex-row">
+        <Link href='#' style={{fontSize: "13px"}} className=" flex flex-row hover:text-sky-600">
                      <AiOutlineSlack size={23} /><span className="mt-1">Party</span>
                     </Link>
 
-                    <Link href='#' style={{fontSize: "13px"}} className=" flex flex-row">
+                    <Link href='#' style={{fontSize: "13px"}} className=" flex flex-row hover:text-orange-500">
                      <span className="mt-1">Nova</span><AiOutlineSlack size={23} /><span className="mt-1">Pay</span>
                     </Link>
         </div>
