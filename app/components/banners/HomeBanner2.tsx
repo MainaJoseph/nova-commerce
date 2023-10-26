827ceadda4a9a4bb8a5a60af80949d3e4d322f29
MainaJoseph <mainajm254@gmail.com>
2023-10-26 15:17:16 +0300

    added Utils and dummy data

--- app/components/banners/HomeBanner2.tsx
+++ app/components/banners/HomeBanner2.tsx
@@ -10,7 +10,7 @@ interface BannerProps {
   const HomeBanner2: React.FC<BannerProps> = ({ onNext, onPrevious }) => {
   return (
     <div className="relative bg-gradient-to-r from-sky-400 to-sky-700 mb-8">
-      <div className="mx-auto px-8 py-12 flex flex-col md:flex-row items-center justify-evenly relative">
+      <div className="mx-auto px-8 py-12 flex flex-col md:flex-col items-center justify-evenly relative">
         <div className="mb-8 md:mb-0 text-center flex-1">
           <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">
             Summer Sale
