0deff410205ff9e4d1597f1c875188d75b19362f
MainaJoseph <mainajm254@gmail.com>
2023-10-26 22:10:34 +0300

    Added singleProduct.ts util

---
+++ utils/singleProduct.ts
@@ -0,0 +1,73 @@
+export const singleProduct = {
+  id: "648437b38c44d52b9542e340",
+  name: "Apple iPhone 12, 64GB",
+  description:
+    'The product is refurbished, fully functional, and in excellent condition. Backed by the 90-day E~Shop Renewed Guarantee.\n- This pre-owned product has been professionally inspected, tested and cleaned by Amazon qualified vendors. It is not certified by Apple.\n- This product is in "Excellent condition". The screen and body show no signs of cosmetic damage visible from 12 inches away.\n- This product will have a battery that exceeds 80% capacity relative to new.\n- Accessories may not be original, but will be compatible and fully functional. Product may come in generic box.\n- Product will come with a SIM removal tool, a charger and a charging cable. Headphone and SIM card are not included.\n- This product is eligible for a replacement or refund within 90-day of receipt if it does not work as expected.\n- Refurbished phones are not guaranteed to be waterproof.',
+  price: 40,
+  brand: "Apple",
+  category: "Phone",
+  inStock: true,
+  images: [
+    {
+      color: "Black",
+      colorCode: "#000000",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/ecommerce-shop-cc542.appspot.com/o/products%2F1686386605041-iphone12-black.jpg?alt=media&token=32abf316-ddf6-4870-9e1e-a7c884b750d6",
+    },
+    {
+      color: "Blue",
+      colorCode: " #0000FF",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/ecommerce-shop-cc542.appspot.com/o/products%2F1686386607274-iphone12-blue.jpg?alt=media&token=e83a9b13-86b6-4518-9f1e-8ddef12ba9a2",
+    },
+    {
+      color: "Red",
+      colorCode: "#FF0000",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/ecommerce-shop-cc542.appspot.com/o/products%2F1686386608652-iphone12-red.jpg?alt=media&token=603a9e86-5b8c-4f8d-b61c-c1c77e60e954",
+    },
+  ],
+  reviews: [
+    {
+      id: "6499b4887402b0efd394d8f3",
+      userId: "6499b184b0e9a8c8709821d3",
+      productId: "648437b38c44d52b9542e340",
+      rating: 4,
+      comment:
+        "good enough. I like the camera and casing. the delivery was fast too.",
+      createdDate: "2023-06-26T15:53:44.483Z",
+      user: {
+        id: "6499b184b0e9a8c8709821d3",
+        name: "Chaoo",
+        email: "example1@gmail.com",
+        emailVerified: null,
+        image:
+          "https://lh3.googleusercontent.com/a/AAcHTtcuRLwWi1vPKaQOcJlUurlhRAIIq2LgYccE8p32=s96-c",
+        hashedPassword: null,
+        createdAt: "2023-06-26T15:40:52.558Z",
+        updatedAt: "2023-06-26T15:40:52.558Z",
+        role: "USER",
+      },
+    },
+    {
+      id: "6499a110efe4e4de451c7edc",
+      userId: "6475af156bad4917456e6e1e",
+      productId: "648437b38c44d52b9542e340",
+      rating: 5,
+      comment: "I really liked it!!",
+      createdDate: "2023-06-26T14:30:40.998Z",
+      user: {
+        id: "6475af156bad4917456e6e1e",
+        name: "Charles",
+        email: "example@gmail.com",
+        emailVerified: null,
+        image:
+          "https://lh3.googleusercontent.com/a/AAcHTteOiCtILLBWiAoolIW9PJH-r5825pBDl824_8LD=s96-c",
+        hashedPassword: null,
+        createdAt: "2023-05-30T08:08:53.979Z",
+        updatedAt: "2023-05-30T08:08:53.979Z",
+        role: "ADMIN",
+      },
+    },
+  ],
+};
