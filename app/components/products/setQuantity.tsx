8762344e71e3e01236626bac0c34c0e7c94ed4be
MainaJoseph <mainajm254@gmail.com>
2023-10-27 08:36:14 +0300

    Ipdated SetQuantity.tsx

--- app/components/products/setQuantity.tsx
+++ app/components/products/setQuantity.tsx
@@ -1,5 +1,36 @@
-const setQuantity = () => {
-  return <div></div>;
+"use client";
+
+import { CartProductType } from "@/app/product/[productid]/ProductDetails";
+
+interface SetQtyProps {
+  cartCounter?: boolean;
+  cartProduct: CartProductType;
+  handleQtyIncrease: () => void;
+  handleQtyDecrease: () => void;
+}
+
+const btnStyles = "border-[1.2px] border-slate-300 px-2 rounded";
+
+const SetQuantity: React.FC<SetQtyProps> = ({
+  cartCounter,
+  cartProduct,
+  handleQtyIncrease,
+  handleQtyDecrease,
+}) => {
+  return (
+    <div className="flex gap-8 items-center">
+      {cartCounter ? null : <div className="font-semibold">QUANTITY</div>}
+      <div className="flex gap-4 items-center text-base">
+        <button onClick={handleQtyDecrease} className={btnStyles}>
+          -
+        </button>
+        <div>{cartProduct.quantity}</div>
+        <button onClick={handleQtyIncrease} className={btnStyles}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default setQuantity;
+export default SetQuantity;
