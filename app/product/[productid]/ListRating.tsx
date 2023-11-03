043d14def78586a3c5972ed68d75c173eb7f168b
MainaJoseph <mainajm254@gmail.com>
2023-11-03 09:16:06 +0300

    Created ListRating.tsx

--- app/product/[productid]/ListRating.tsx
+++ app/product/[productid]/ListRating.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import Heading from "@/app/components/Heading";
+
 interface ListRatingProps {
   product: any;
 }
 
 const ListRating: React.FC<ListRatingProps> = ({ product }) => {
-  return <div></div>;
+  return (
+    <div>
+      <Heading title="Product Review" />
+    </div>
+  );
 };
 
 export default ListRating;
