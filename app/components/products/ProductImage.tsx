2375e39b5c04e5d41c862c63d16c91e90a3c9ada
MainaJoseph <mainajm254@gmail.com>
2023-11-03 08:36:48 +0300

    Hooked Product image to Product Details

--- app/components/products/ProductImage.tsx
+++ app/components/products/ProductImage.tsx
@@ -4,6 +4,8 @@ import {
   CartProductType,
   SelectedImgType,
 } from "@/app/product/[productid]/ProductDetails";
+import Image from "next/image";
+import { Key } from "react";
 
 interface ProductImageProps {
   cartProduct: CartProductType;
@@ -17,8 +19,31 @@ const ProductImage: React.FC<ProductImageProps> = ({
   handleColorSelect,
 }) => {
   return (
-    <div className="grid grid-cols-6 gap-2 h-full max-h-[500px] min-h-[300px] sm:m-h-[400px]">
-      <div></div>
+    <div className="grid grid-cols-6 gap-2 h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
+      <div className="flex flex-col items-center justify-center gap-4 cursor-pointer border h-ful max-h-[500px] min-h-[300px] sm:min-h-[400px]">
+        {product.images.map((image: SelectedImgType) => {
+          return (
+            <div
+              key={image.color}
+              onClick={() => {
+                handleColorSelect(image);
+              }}
+              className={`relative w-[80%] aspect-square rounded border-teal-300 ${
+                cartProduct.selectedImg.color === image.color
+                  ? "border-[1.5px]"
+                  : "border-none"
+              }`}
+            >
+              <Image
+                src={image.image}
+                alt={image.color}
+                fill
+                className="object-contain"
+              />
+            </div>
+          );
+        })}
+      </div>
       <div></div>
     </div>
   );
--- app/product/[productid]/ProductDetails.tsx
+++ app/product/[productid]/ProductDetails.tsx
@@ -5,6 +5,7 @@ import { Rating } from "@mui/material";
 import SetColor from "@/app/components/products/SetColor";
 import SetQuantity from "@/app/components/products/SetQuantity";
 import Button from "@/app/components/Button";
+import ProductImage from "@/app/components/products/ProductImage";
 
 interface ProductDetailsProps {
   product: any;
@@ -77,7 +78,11 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
 
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-12">
-      <div>Images</div>
+      <ProductImage
+        cartProduct={cartProduct}
+        product={product}
+        handleColorSelect={handleColorSelect}
+      />
       <div className="flex flex-col gap-1 text-slate-500 text-sm">
         <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
         <div className="flex items-center gap-2">
