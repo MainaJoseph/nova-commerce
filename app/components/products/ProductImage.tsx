5517b673d5051128bb34c0643756da43f4102321
MainaJoseph <mainajm254@gmail.com>
2023-11-03 08:44:24 +0300

    Added main product details image

--- app/components/products/ProductImage.tsx
+++ app/components/products/ProductImage.tsx
@@ -44,7 +44,14 @@ const ProductImage: React.FC<ProductImageProps> = ({
           );
         })}
       </div>
-      <div></div>
+      <div className="col-span-5 relative aspect-square">
+        <Image
+          fill
+          src={cartProduct.selectedImg.image}
+          alt={cartProduct.name}
+          className="w-full h-full object-contain max-h-[500px] min-h-[300px] sm:min-h-[400px]"
+        />
+      </div>
     </div>
   );
 };
