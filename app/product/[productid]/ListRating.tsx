1c35800c51f7a504c42e3858e0d065f2a19040af
MainaJoseph <mainajm254@gmail.com>
2023-11-03 09:35:11 +0300

    Added comment sections in the reviews

--- app/product/[productid]/ListRating.tsx
+++ app/product/[productid]/ListRating.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Heading from "@/app/components/Heading";
+import { Rating } from "@mui/material";
+import moment from "moment";
 
 interface ListRatingProps {
   product: any;
@@ -10,6 +12,27 @@ const ListRating: React.FC<ListRatingProps> = ({ product }) => {
   return (
     <div>
       <Heading title="Product Review" />
+      <div className="text-sm mt-2">
+        {product.reviews &&
+          product.reviews.map((review: any) => {
+            return (
+              <div key={review.id} className="max-w-[300px]">
+                <div className="flex gap-2 items-center">
+                  <div>Avatar</div>
+                  <div className="font-semibold">{review?.user.name}</div>
+                  <div className="font-light">
+                    {moment(review.CreatedDate).fromNow()}
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <Rating value={review.rating} readOnly />
+                  <div className="ml-2">{review.comment}</div>
+                  <hr className="mt-4 mb-4" />
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
