168ad3954ae21236ec3096be55e92d7ee4597db5
MainaJoseph <mainajm254@gmail.com>
2023-11-07 07:46:48 +0300

    Structured the toast in the layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import NavBar from "./components/nav/NavBar";
 import Footer from "./components/footer/Footer";
 import Navy from "./components/navy/Navy";
 import CartProvider from "@/providers/CartProvider";
+import toast, { Toaster } from "react-hot-toast";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -21,6 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className} text-slate-700`}>
+        <Toaster
+          toastOptions={{
+            style: {
+              background: "rgb(51 65 85)",
+              color: "#fff",
+            },
+          }}
+        />
         <CartProvider>
           <div className="flex flex-col min-h-screen">
             <NavBar />
