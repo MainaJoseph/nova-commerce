a1e67bc40c2b7b2505680e9cf646a37fe5b55d06
MainaJoseph <mainajm254@gmail.com>
2023-11-09 09:51:42 +0300

    Creating an Input field

---
+++ app/components/input/Input.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+}
+
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  type,
+  disabled,
+  required,
+  register,
+  errors,
+}) => {
+  return <div></div>;
+};
+
+export default Input;
