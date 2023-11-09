e710d00f8a8acdac15134938b9c8686bc899e018
MainaJoseph <mainajm254@gmail.com>
2023-11-09 15:20:32 +0300

    Added Nova icon to the signup page

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { FcEngineering } from "react-icons/fc";
 import Heading from "../components/Heading";
 
 const RegisterForm = () => {
   return (
     <>
-      <Heading title="Sign Up For Nova" />
+      <div className="flex gap-1 items-center">
+        <Heading title="Sign Up For Nova" />
+        <FcEngineering size={24} className="mt-2" />
+      </div>
+
       <hr
         className="bg-orange-300
       w-full
