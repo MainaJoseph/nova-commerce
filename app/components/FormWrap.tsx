6c944ebc907f4aad35626e4245275b262f4611b8
MainaJoseph <mainajm254@gmail.com>
2023-11-09 14:57:00 +0300

    created formwrap

---
+++ app/components/FormWrap.tsx
@@ -0,0 +1,33 @@
+const FormWrap = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div
+      className="
+  m-h-fit
+  h-full
+  flex
+  items-center
+  justify-center
+  pb-12
+  pt-24"
+    >
+      <div
+        className="
+      max-w-[650px]
+      w-full
+      flex
+      flex-col
+      g-6
+      items-center
+      shadow-xl
+      shadow-slate-200
+      rounded-md
+      p-4
+      md:p-8"
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default FormWrap;
