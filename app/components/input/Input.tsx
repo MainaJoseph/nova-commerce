93cf908953b9f9c396e04395cacdb4d38fbb21f7
MainaJoseph <mainajm254@gmail.com>
2023-11-09 11:50:04 +0300

    Finished the input field

--- app/components/input/Input.tsx
+++ app/components/input/Input.tsx
@@ -21,7 +21,40 @@ const Input: React.FC<InputProps> = ({
   register,
   errors,
 }) => {
-  return <div></div>;
+  return (
+    <div className="w-full relative">
+      <input
+        autoComplete="off"
+        id={id}
+        disabled={disabled}
+        {...register(id, { required })}
+        placeholder=""
+        type={type}
+        className={`peer w-ful p-4 pt-6 outline-none bg-white font-light border-2 rounded-md 
+      transition disabled:opacity-70 disabled:cursor-not-allowed 
+      ${errors[id] ? "border-rose-400" : "border-slate-00"}
+      ${errors[id] ? "focus:border-rose-400" : "focus:border-slate-300"}`}
+      />
+      <label
+        htmlFor={id}
+        className="absolute 
+                cursor-text 
+                text-md duration-150
+                transform 
+                -translate-y-3
+                top-5 
+                 z-10
+                 origin-[0]
+                 left-4
+                peer-placeholder-shown: scale-100
+                peer-placeholder-shown:translate-y-0  
+                peer-focus:scale-75
+                peer-focus:translate-y-4"
+      >
+        {label}
+      </label>
+    </div>
+  );
 };
 
 export default Input;
