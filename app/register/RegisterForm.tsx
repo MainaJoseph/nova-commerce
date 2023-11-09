50dc9ab48ca3a986f2f423edd27e2694ed795012
MainaJoseph <mainajm254@gmail.com>
2023-11-09 15:07:59 +0300

    Added Register Form

---
+++ app/register/RegisterForm.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Heading from "../components/Heading";
+
+const ResgisterForm = () => {
+  return (
+    <>
+      <Heading title="Sign Up For Nova" />
+      <hr
+        className="bg-slate-300
+      w-full
+      h-px"
+      />
+    </>
+  );
+};
+
+export default ResgisterForm;
