d28d6c01adb3832ac2d93a24db4664c0f86a020b
MainaJoseph <mainajm254@gmail.com>
2023-11-10 12:51:37 +0300

    Created a BackDrop

---
+++ app/components/nav/BackDrop.tsx
@@ -0,0 +1,14 @@
+interface BackDropProps {
+  onClick: () => void;
+}
+
+const BackDrop: React.FC<BackDropProps> = ({ onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className="z-20 bg-slate-200 opacity-50 w-screen h-screen fixed t-0 left-0"
+    ></div>
+  );
+};
+
+export default BackDrop;
