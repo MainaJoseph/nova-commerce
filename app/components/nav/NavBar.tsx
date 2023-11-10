5b179d89b1ef14262eb4d5d9160648eaf2ee6f01
MainaJoseph <mainajm254@gmail.com>
2023-11-10 12:18:07 +0300

    Added UserMenu to NavBar

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -3,6 +3,7 @@ import Container from "../Container";
 import { Redressed } from "next/font/google";
 import { FcEngineering } from "react-icons/fc";
 import CartCount from "./CartCount";
+import UserMenu from "./UserMenu";
 
 const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
@@ -33,7 +34,7 @@ const NavBar = () => {
             <div className="hidden md:block">Search</div>
             <div className="flex items-center gap-8 md:gap-12">
               <CartCount />
-              <div>Usermenu</div>
+              <UserMenu />
             </div>
           </div>
         </Container>
--- app/components/nav/UserMenu.tsx
+++ app/components/nav/UserMenu.tsx
@@ -1,5 +1,30 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import Avatar from "../Avatar";
+import { AiFillCaretDown } from "react-icons/ai";
+
 const UserMenu = () => {
-  return <></>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toogleOpen = useCallback(() => {
+    setIsOpen((prev) => !prev);
+  }, []);
+
+  return (
+    <>
+      <div className="relative z-30">
+        <div
+          onClick={toogleOpen}
+          className="p-2 border-[1px] border-orange-400 flex flex-row items-center gap-1 rounded-full 
+        cursor-pointer hover:shadow-md transition text-slate-700"
+        >
+          <Avatar />
+          <AiFillCaretDown />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserMenu;
