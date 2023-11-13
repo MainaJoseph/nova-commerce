d85e294c7ddbf15b0d23a5e4686cfc71f8390619
MainaJoseph <mainajm254@gmail.com>
2023-11-13 19:05:13 +0300

    added chechout

--- app/checkout/CheckOutClient.tsx
+++ app/checkout/CheckOutClient.tsx
@@ -1,5 +1,55 @@
+"use client";
+
+import { useCart } from "@/hooks/useCart";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
 const CheckOutClient = () => {
-  return <></>;
+  const { cartProducts, paymentIntent, handleSetPaymentIntent } = useCart();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [clientSecret, setClientSecret] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    //Create a payment Intent as soon as the Page Loads
+
+    if (cartProducts) {
+      setLoading(true);
+      setError(false);
+
+      fetch("/api/create-payment-intent", {
+        method: "POST",
+        headers: { "Content-Type": "application-json" },
+        body: JSON.stringify({
+          items: cartProducts,
+          payment_intent_id: paymentIntent,
+        }),
+      })
+        .then((res) => {
+          setLoading(false);
+
+          if (res.status === 401) {
+            return router.push("/login");
+          }
+
+          return res.json();
+        })
+        .then((data) => {
+          setClientSecret(data.paymentIntent.clientSecret);
+          handleSetPaymentIntent(data.paymentIntent.id);
+        })
+        .catch((error) => {
+          setError(true);
+          console.log("Error", error);
+          toast.error("Something went Wrong");
+        });
+    }
+  }, [cartProducts, paymentIntent, router, handleSetPaymentIntent]);
+
+  return <>CheckOut</>;
 };
 
 export default CheckOutClient;
