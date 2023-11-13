c77d8e63ebd35453b2c284c512f53d096360cc97
MainaJoseph <mainajm254@gmail.com>
2023-11-13 15:57:01 +0300

    Added currentUser Avatar image

--- app/components/nav/UserMenu.tsx
+++ app/components/nav/UserMenu.tsx
@@ -28,7 +28,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
           className="p-2 border-[1px] border-orange-400 flex flex-row items-center gap-1 rounded-full 
         cursor-pointer hover:shadow-md transition text-slate-700"
         >
-          <Avatar />
+          <Avatar src={currentUser?.image} />
           <AiFillCaretDown />
         </div>
         {isOpen && (
