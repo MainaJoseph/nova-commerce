7726a82bd22fdac8f7b229d6b30040312295f16f
MainaJoseph <mainajm254@gmail.com>
2023-11-18 15:51:02 +0300

    added const price to return to two decimal points

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -15,7 +15,9 @@ const calculateOrderAmount = (items: CartProductType[]) => {
     return acc + itemTotal;
   }, 0);
 
-  return totalPrice;
+  const price: any = totalPrice.toFixed(2);
+
+  return price;
 };
 
 export async function POST(request: Request) {
