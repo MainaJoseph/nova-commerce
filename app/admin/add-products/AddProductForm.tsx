89f78a62c36c648e5cca1f1eb1a439c974bc9806
MainaJoseph <mainajm254@gmail.com>
2023-11-29 10:07:55 +0300

    added component CustomCheckBox

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -2,8 +2,10 @@
 
 import Heading from "@/app/components/Heading";
 import Input from "@/app/components/input/Input";
+import TextArea from "@/app/components/input/TextArea";
 import { useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import { FcEngineering } from "react-icons/fc";
 
 const AddProductForm = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -28,32 +30,46 @@ const AddProductForm = () => {
 
   return (
     <>
-      <Heading title="Add a Product" center color="orange" />
-
-      <Input
-        id="name"
-        label="Name"
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-      />
-
-      <Input
-        id="price"
-        label="Price"
-        disabled={isLoading}
-        register={register}
-        type="number"
-        errors={errors}
-      />
-
-      <Input
-        id="brand"
-        label="Brand"
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-      />
+      <div className="space-y-4 w-full">
+        {" "}
+        {/* Add a container with spacing */}
+        <div className="flex gap-4 items-center justify-center">
+          {" "}
+          {/* Add spacing to the heading and icon */}
+          <Heading title="Add Product" center />
+          <FcEngineering size={24} className="mt-0" />
+        </div>
+        <hr className="bg-orange-500 w-full h-px" />
+        <Input
+          id="name"
+          label="Name"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+        />
+        <Input
+          id="price"
+          label="Price"
+          disabled={isLoading}
+          register={register}
+          type="number"
+          errors={errors}
+        />
+        <Input
+          id="brand"
+          label="Brand"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+        />
+        <TextArea
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+        />
+      </div>
     </>
   );
 };
