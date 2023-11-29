7a24bee8f3c999e2cab4cd9cb034dcc022dd38d3
MainaJoseph <mainajm254@gmail.com>
2023-11-29 08:27:28 +0300

    added component NullData to return errors

---
+++ app/admin/add-products/AddProductForm.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const AddProductForm = () => {
+  return <div></div>;
+};
+
+export default AddProductForm;
--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -1,5 +1,19 @@
-const AddProducts = () => {
-  return <div>Add Products</div>;
+import Container from "@/app/components/Container";
+import FormWrap from "@/app/components/FormWrap";
+import AddProductForm from "./AddProductForm";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+
+const AddProducts = async () => {
+  const currentUser = await getCurrentUser();
+  return (
+    <div className="p-8">
+      <Container>
+        <FormWrap>
+          <AddProductForm />
+        </FormWrap>
+      </Container>
+    </div>
+  );
 };
 
 export default AddProducts;
