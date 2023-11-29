7bdad3657a74b8094a193d0bde4a2af449a11f41
MainaJoseph <mainajm254@gmail.com>
2023-11-29 08:08:27 +0300

    added firebase and product route

---
+++ app/api/product/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/libs/prismadb";
+import { NextResponse } from "next/server";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+  const { name, description, price, brand, category, inStock, images } = body;
+
+  const product = await prisma.product.create({
+    data: {
+      name,
+      description,
+      brand,
+      category,
+      inStock,
+      images,
+      price: parseFloat(price),
+    },
+  });
+
+  return NextResponse.json(product);
+}
