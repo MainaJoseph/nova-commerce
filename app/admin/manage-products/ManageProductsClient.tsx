b30c1909879c625fffd4496bd6272cd3b485be93
MainaJoseph <mainajm254@gmail.com>
2024-02-28 01:36:57 +0300

    Added instock toggle

--- app/admin/manage-products/ManageProductsClient.tsx
+++ app/admin/manage-products/ManageProductsClient.tsx
@@ -4,6 +4,19 @@ import { Product } from "@prisma/client";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import { FormatPrice } from "@/utils/FormatPrice";
 import Heading from "@/app/components/Heading";
+import Status from "@/app/components/Status";
+import {
+  MdCached,
+  MdClose,
+  MdDelete,
+  MdDone,
+  MdRemoveRedEye,
+} from "react-icons/md";
+import ActionsBtn from "@/app/components/ActionsBtn";
+import { useCallback } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 interface ManageProductsClientProps {
   products: Product[];
@@ -12,6 +25,7 @@ interface ManageProductsClientProps {
 const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
   products,
 }) => {
+  const router = useRouter();
   let rows: any = [];
 
   if (products) {
@@ -29,8 +43,8 @@ const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
   }
 
   const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 200 },
-    { field: "name", headerName: "Name", width: 220 },
+    { field: "id", headerName: "ID", width: 220 },
+    { field: "name", headerName: "Name", width: 180 },
     {
       field: "price",
       headerName: "Price(Kes)",
@@ -47,23 +61,69 @@ const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
       width: 120,
       renderCell: (params) => {
         return (
-          <div>{params.row.inStock === true ? "instock" : "out of stock"}</div>
+          <div>
+            {params.row.inStock === true ? (
+              <Status
+                text="InStock"
+                icon={MdDone}
+                bg="bg-teal-200"
+                color="text-teal-700"
+              />
+            ) : (
+              <Status
+                text="Out of Stock"
+                icon={MdClose}
+                bg="bg-rose-200"
+                color="text-rose-700"
+              />
+            )}
+          </div>
         );
       },
     },
     {
       field: "action",
       headerName: "Actions",
-      width: 200,
+      width: 170,
       renderCell: (params) => {
-        return <div>Actions</div>;
+        return (
+          <div className="flex justify-between gap-4 w-full">
+            <ActionsBtn
+              icon={MdCached}
+              onClick={() => {
+                handleToggleStatus(params.row.id, params.row.inStock);
+              }}
+            />
+            <ActionsBtn icon={MdDelete} onClick={() => {}} />
+            <ActionsBtn icon={MdRemoveRedEye} onClick={() => {}} />
+          </div>
+        );
       },
     },
   ];
 
+  const handleToggleStatus = useCallback(
+    (id: string, inStock: boolean) => {
+      axios
+        .put("/api/product", {
+          id,
+          inStock: !inStock,
+        })
+        .then((res) => {
+          toast.success("Product Status Changed");
+          router.refresh();
+        })
+        .catch((err) => {
+          toast.error("OPPS!..Something went wrong");
+          console.log("err");
+        });
+    },
+    [router]
+  );
+
   return (
-    <div className="max-w-[1150px] m-auto text-xl">
-      <div className="mb-4 mt-8">
+    <div className="max-w-[1250px] m-auto text-xl">
+      <div className="mb-4 mt-4">
         <Heading title="Manage Products" center />
       </div>
       <div style={{ height: 600, width: "100%" }}>
@@ -77,6 +137,7 @@ const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
           }}
           pageSizeOptions={[9, 20]}
           checkboxSelection
+          disableRowSelectionOnClick
         />
       </div>
     </div>
--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -26,3 +26,21 @@ export async function POST(request: Request) {
 
   return NextResponse.json(product);
 }
+
+export async function PUT(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+  const { id, inStock } = body;
+
+  const product = await prisma.product.update({
+    where: { id: id },
+    data: { inStock },
+  });
+
+  return NextResponse.json(product);
+}
