36c0da4c10e0f8af42ea949cfbe516a72434f0ec
MainaJoseph <mainajm254@gmail.com>
2024-03-12 12:00:23 +0300

    Fixed get currentUser

--- actions/getCurrentUser.ts
+++ actions/getCurrentUser.ts
@@ -26,8 +26,7 @@ export async function getCurrentUser() {
 
     return {
       ...currentUser,
-      createdAt: currentUser.createdAt.toISOString(),
-      updatedAt: currentUser.updatedAt.toISOString(),
+
       emailVerified: currentUser.emailVerified?.toISOString() || null,
     };
   } catch (error: any) {
