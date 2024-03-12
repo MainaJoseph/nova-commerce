690e5a9c8ab72f7612b9922b66a5df28fcd26afe
MainaJoseph <mainajm254@gmail.com>
2024-03-12 13:12:38 +0300

    protected admin page route from unauthorized access

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,11 +3,19 @@ import Summary from "./Summary";
 import getOrders from "@/actions/getOrder";
 import getUsers from "@/actions/getUsers";
 import Container from "../components/Container";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "../components/NullData";
 
 const Admin = async () => {
   const products = await getProducts({ category: null });
   const orders = await getOrders();
   const users = await getUsers();
+
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Opps! Access Denied" />;
+  }
 
   return (
     <div className="pt-8">
