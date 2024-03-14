932dd19ea5a9b0bd0339abd08badc2c1b04fc24b
MainaJoseph <mainajm254@gmail.com>
2024-03-14 18:26:22 +0300

    Fixed to add to cart if product is in stock

--- app/product/[productid]/ProductDetails.tsx
+++ app/product/[productid]/ProductDetails.tsx
@@ -120,7 +120,7 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
           <span className="font-semibold">BRAND:</span> {product.brand}
         </div>
         <div className={product.inStock ? "text-teal-400" : "text-rose-400"}>
-          {product.inStock ? "in Stock" : "out of Stock"}
+          {product.inStock ? "in Stock ✅" : "out of Stock ❌"}
         </div>
         <Horizontal />
         {isProductInCart ? (
