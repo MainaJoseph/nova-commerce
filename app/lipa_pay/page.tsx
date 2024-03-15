8db3bc8fee1a19b122f849217210d57ec6f4b4cb
MainaJoseph <mainajm254@gmail.com>
2024-03-15 10:48:27 +0300

    If no current user Return Nulldata Mpesa page

--- app/lipa_pay/page.tsx
+++ app/lipa_pay/page.tsx
@@ -1,15 +1,20 @@
 export const metadata = {
-  title: "Sign In",
-  description: "Sign In to Nova",
+  title: "lipa_pay",
+  description: "Lipa na Mpesa",
 };
 
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import Container from "../components/Container";
 import FormWrap from "../components/FormWrap";
 import PayForm from "./PayForm";
+import NullData from "../components/NullData";
 
 const Login = async () => {
   const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return <NullData title="Opps! Access Denied" />;
+  }
   return (
     <Container>
       <FormWrap>
