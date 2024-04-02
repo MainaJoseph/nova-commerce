572284a18277db5277ac485aef9d320c988d1847
MainaJoseph <mainajm254@gmail.com>
2024-04-02 12:33:16 +0300

    Addded a pageTracker from shadcn named breadcrumbs

--- app/lipa_pay/PayForm.tsx
+++ app/lipa_pay/PayForm.tsx
@@ -93,7 +93,10 @@ const PayForm: React.FC<PayFormProps> = () => {
     setIsLoading(true);
     console.log("Data>>>>>", data);
     try {
-      const response = await axios.post("/api/lipa/stkpush", data);
+      const response = await axios.post(
+        "/https://lipa-three.vercel.app/stkpush",
+        data
+      );
       if (response.status === 200) {
         toast.success("Payment successful!");
       } else {
