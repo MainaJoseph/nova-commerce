254111666b6eee754e5939c1262c873052521830
MainaJoseph <mainajm254@gmail.com>
2024-04-10 02:31:25 +0300

    Redifine Mover component

--- app/components/carousel-cat/Mover.tsx
+++ app/components/carousel-cat/Mover.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
@@ -10,29 +9,56 @@ import {
 } from "@/components/ui/carousel";
 import Container from "../Container";
 import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
+import Image from "next/image";
 
 export function Mover() {
+  const cardImages = [
+    "/phones.jpg",
+    "/tv.jpg",
+    "/apliance.png",
+    "/beauty.jpg",
+    "/fashion.jpg",
+    "/computing.jpg",
+    "/super.jpg",
+    "/Computing.png",
+    "/Computing.png",
+    "/Computing.png",
+  ];
+
   const cardTexts = [
     "Phone & Tablets",
     "TVs & Audio",
     "Appliances",
-    "Heath and Beuty",
+    "Health and Beauty",
     "Fashion",
     "Computing",
     "Supermarket",
-    "Spoting",
+    "Sporting",
     "Automobile",
     "Others",
   ];
 
+  // Define an array of background colors
+  const cardColors = [
+    "bg-slate-100",
+    "bg-blue-400",
+    "bg-amber-200",
+    "bg-red-200",
+    "bg-gray-200",
+    "bg-orange-400",
+    "bg-gray-200",
+    "bg-teal-200",
+    "bg-indigo-200",
+    "bg-cyan-200",
+  ];
+
   return (
-    <div className="w-full max-w-screen-xl mx-auto">
+    <div className="w-full max-w-screen-xl mx-auto relative overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent via-white to-transparent opacity-50 z-10"></div>
       <Container>
         <Carousel
-          opts={{
-            align: "start",
-          }}
-          className="w-11/12 md:w-4/5 lg:w-3/4"
+          opts={{ align: "start" }}
+          className="w-11/12 md:w-4/5 lg:w-3/4 relative z-20"
         >
           <CarouselContent>
             {Array.from({ length: 10 }).map((_, index) => (
@@ -41,11 +67,16 @@ export function Mover() {
                 className="basis-1/3 md:basis-1/4 lg:basis-1/6"
               >
                 <div className="p-1">
-                  <Card className="cursor-pointer bg-amber-200">
+                  <Card className={`cursor-pointer ${cardColors[index]}`}>
                     <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-3xl font-semibold">
-                        {index + 1}
-                      </span>
+                      <div className="w-full h-full relative overflow-hidden transform scale-y-[1]">
+                        <Image
+                          src={cardImages[index]}
+                          alt={cardTexts[index]}
+                          layout="fill"
+                          objectFit="cover"
+                        />
+                      </div>
                     </CardContent>
                   </Card>
                 </div>
