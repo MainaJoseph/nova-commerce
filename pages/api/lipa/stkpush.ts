9f71386790789a1248fbb7dc3053b133be261b51
MainaJoseph <mainajm254@gmail.com>
2024-04-22 15:16:00 +0300

    Fix Lipa api

--- pages/api/lipa/stkpush.ts
+++ pages/api/lipa/stkpush.ts
@@ -9,6 +9,7 @@ export default async function handler(
   try {
     // Retrieve PartyA (phone) and Amount from the request body
     const { PartyA, Amount } = req.body;
+    const accessToken: string = await getAccessToken();
 
     // Make API call to Safaricom MPesa API
     const response = await axios.post(
@@ -30,7 +31,7 @@ export default async function handler(
       {
         headers: {
           "Content-Type": "application/json",
-          Authorization: "Bearer jdAQSvaQq8xF2zvgAJikAWGHHCZm",
+          Authorization: `Bearer ${accessToken}`, // Include the access token
         },
       }
     );
@@ -46,5 +47,28 @@ export default async function handler(
   } catch (error) {
     console.error("Error processing payment:", error);
     res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+async function getAccessToken(): Promise<string> {
+  const consumer_key = process.env.CONSUMER_KEY as string;
+  const consumer_secret = process.env.CONSUMER_SECRET as string;
+  const url: string =
+    "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+
+  const auth: string =
+    "Basic " +
+    Buffer.from(consumer_key + ":" + consumer_secret).toString("base64");
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: auth,
+      },
+    });
+    const accessToken: string = response.data.access_token;
+    return accessToken;
+  } catch (error) {
+    throw error;
   }
 }
