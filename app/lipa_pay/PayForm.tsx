105cf3f66fb3fe2fce3020c4333f01c705a437e5
MainaJoseph <mainajm254@gmail.com>
2024-04-22 15:08:23 +0300

    Fix Lipa api

--- app/lipa_pay/PayForm.tsx
+++ app/lipa_pay/PayForm.tsx
@@ -93,16 +93,10 @@ const PayForm: React.FC<PayFormProps> = () => {
     setIsLoading(true);
     console.log("Data>>>>>", data);
     try {
-      const response = await axios.post(
-        "/api/lipa/stkpush",
-        {
-          PartyA: data.phone, // Use phone value from the form
-          Amount: cartTotalAmount, // Use cartTotalAmount as the amount
-        },
-        {
-          timeout: 10000, // Specify the timeout duration in milliseconds (e.g., 10 seconds)
-        }
-      );
+      const response = await axios.post("/api/lipa/stkpush", {
+        PartyA: data.phone, // Use phone value from the form
+        Amount: cartTotalAmount, // Use cartTotalAmount as the amount
+      });
       if (response.status === 200) {
         toast.success("Payment successful!");
       } else {
