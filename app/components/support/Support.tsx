24d1e082b96e6c6734a1d9b189f970ae422f578d
MainaJoseph <mainajm254@gmail.com>
2024-05-02 14:38:25 +0300

    Changed code format

--- app/components/support/Support.tsx
+++ app/components/support/Support.tsx
@@ -1,67 +1,16 @@
-import SupportWidget from "./SupportWidget";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { getCurrentUser } from "@/actions/getCurrentUser";
 import { SafeUser } from "@/types";
-import Login from "@/app/login/page";
-import SupportLogin from "./SupportLogin";
-import Heading from "../Heading";
-import Chat from "./Chat";
+import SheetClient from "./SheetClient";
 
-interface Props {
-  currentUser: SafeUser | null; // Update this type definition as per your user data structure
+interface UserMenuProps {
+  currentUser: SafeUser | null;
 }
 
-const Support = ({ currentUser }: Props) => {
+const Support: React.FC<UserMenuProps> = ({ currentUser }) => {
   return (
     <div>
-      <Sheet>
-        <SheetTrigger>
-          <SupportWidget />
-        </SheetTrigger>
-        <SheetContent className="bg-white border-slate-800">
-          {currentUser ? (
-            <SheetHeader>
-              <SheetTitle>Chat with Nova</SheetTitle>
-              <SheetDescription className="mt-3">
-                Welcome to our support! How can we assist you today?
-                <Chat />
-              </SheetDescription>
-            </SheetHeader>
-          ) : (
-            // Conditional Render
-            <div className="p-2 flex flex-col">
-              <div>
-                <Heading title="Please Login to contact support" />
-              </div>
-              <div className="mt-20">
-                <SupportLogin currentUser={currentUser} />
-              </div>
-            </div>
-          )}
-          <SheetFooter className="mt-8 flex-grow items-center justify-center">
-            {" "}
-            {/* Updated class */}
-            <p className="mt-4 hover:text-orange-500 text-center">
-              &copy; {new Date().getFullYear()} Nova. All rights reserved
-            </p>
-          </SheetFooter>
-        </SheetContent>
-      </Sheet>
+      <SheetClient currentUser={currentUser} />
     </div>
   );
 };
-
-export async function getServerSideProps() {
-  const currentUser = await getCurrentUser();
-  return { props: { currentUser } };
-}
 
 export default Support;
