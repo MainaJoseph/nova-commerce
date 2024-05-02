6d50400f5f142b54edbf7beb0d664d1ac6f7c9f9
MainaJoseph <mainajm254@gmail.com>
2024-05-02 14:12:13 +0300

    Made adjustments to to phone image

--- app/components/playstore/Template.tsx
+++ app/components/playstore/Template.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Template = () => {
   return (
-    <div className="w-full mb-3 md:mb-[-30px] md:z-10">
+    <div className="w-full mb-3 md:mt-4 md:mb-[-50px] md:z-10">
       <Image
         className="mx-auto max-w-full h-auto"
         src="/scrop.png"
