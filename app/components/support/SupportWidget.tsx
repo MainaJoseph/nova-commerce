e0945a1c5998087351fed555f3ba6e5478cf6c18
MainaJoseph <mainajm254@gmail.com>
2024-05-02 15:59:43 +0300

    Made widget look good

--- app/components/support/SupportWidget.tsx
+++ app/components/support/SupportWidget.tsx
@@ -8,7 +8,7 @@ const SupportWidget = () => {
     <div className="fixed bottom-5 right-5 z-50">
       <BiSupport
         size={45}
-        className="bg-orange-500 text-white rounded-full shadow-lg hover:bg-orange-300 focus:outline-none"
+        className="bg-orange-500 text-white rounded-full shadow-lg hover:bg-orange-300 focus:outline-none rounded-bl-sm"
       />
     </div>
   );
