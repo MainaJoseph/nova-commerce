808be8734ef8cfa565b9db7e4cb274c06df9fad1
MainaJoseph <mainajm254@gmail.com>
2024-05-26 03:30:26 +0300

    Added agent view order by id

--- app/agent/view-orders/order/[orderId]/page.tsx
+++ app/agent/view-orders/order/[orderId]/page.tsx
@@ -2,6 +2,7 @@ import Container from "@/app/components/Container";
 import OrderDetails from "./OrderDetails";
 import getOrderById from "@/actions/getOrderById";
 import NullData from "@/app/components/NullData";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
 interface IParams {
   orderId?: string; // Changed property name to match the one used in getOrderById
@@ -9,8 +10,16 @@ interface IParams {
 
 const Order = async ({ params }: { params: IParams }) => {
   const order = await getOrderById(params);
+  const currentUser = await getCurrentUser();
 
   if (!order) return <NullData title="No order"></NullData>;
+
+  if (
+    !currentUser ||
+    (currentUser.role !== "ADMIN" && currentUser.role !== "AGENT")
+  ) {
+    return <NullData title="Opps! Access Denied" />;
+  }
 
   return (
     <div className="p-8">
