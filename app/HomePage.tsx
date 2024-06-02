bf70ca4de00b31de5994cd72e53dbe1e704516d2
MainaJoseph <mainajm254@gmail.com>
2024-06-03 00:39:31 +0300

    Fixed shuffle algorithm

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -21,14 +21,12 @@ export default async function HomePage({ searchParams }: HomeProps) {
     );
   }
 
-  // Fisher-yates shuffle algorithm
-
-  function shuffleArray(array: any) {
+  // Fisher-Yates shuffle algorithm
+  function shuffleArray(array: any[]) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [array[i], (array[j] = array[j]), array[i]];
+      [array[i], array[j]] = [array[j], array[i]];
     }
-
     return array;
   }
 
@@ -39,7 +37,7 @@ export default async function HomePage({ searchParams }: HomeProps) {
       <Container>
         <Slider />
       </Container>
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl-grid-cols-5 2xl:grid-cols-6 gap-8">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {shuffledProducts.map((product: any) => {
           return <ProductCard data={product} key={product.id} />;
         })}
